b37e7a625794f763a8ead7c88ef84c19b0c47d44
Pett97 <henriquepeterson138@gmail.com>
2024-10-09 00:37:40 -0300

    atividade ok

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View,Image } from "react-native";
+
+let pets = [
+  {
+    animal: "cao",
+    name: "sarnento",
+    age: 2,
+  },
+  {
+    animal: "cat",
+    name: "jurubeba",
+    age: 1,
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {pets.map((pet, index) => (
+        <View key={index}>
+          {pet.animal === "cao" ? (
+           <>
+            <Text>
+              Who let the dogs out: {pet.name}, {pet.age}
+            </Text>
+            <Image source={require("./assets/images/doguinho_caramelo.jpeg")}/>
+           </>
+          ) : (
+            <Text>Are we in Jumanji {pet.name}?</Text>
+          )}
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +38,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  animalContainer: {
+    marginBottom: 20,
+    alignItems: "center",
   },
 });
